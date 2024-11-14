fad2e1c33d9908dd1b5da8e2123fd477676573e9
Megan Chiu <megan.chiu.2014@gmail.com>
2024-11-14 13:55:34 -0800

    add comments throughout code to explain each step

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ Pseudocode for Block 7 Workshop: Secure the Vault
 5. Display an alert with the vault combination.
 */
 
+// Create a string for the user
 const userInstruction =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
+// Declare first variable and perform calculation using + operator
 let firstVar = 2 + 8;
+
+// Declare second variable and perform calculation using / operator
 let secondVar = 80 / 2;
+
+// Declare third variable and perform calculation using * operator
 let thirdVar = 13 * 3;
 
+// Create popup dialog displaying the string and vault combination
 alert(userInstruction + " " + firstVar + "-" + secondVar + "-" + thirdVar);
